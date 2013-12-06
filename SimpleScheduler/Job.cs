a22090f0d8cd82718f140d4209b2c9e4856d924d
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2013-12-05 23:09:24 -0500

    Readd all the files.

---
+++ SimpleScheduler/Job.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleScheduler
+{
+    internal class Job
+    {
+        public Guid Id { get; set; }
+        public Action Action { get; set; }
+        public double MillisecondsToTrigger { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
